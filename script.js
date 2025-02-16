c00f3de7d366bacec135b43e4362deaa01ac1468
Joskemu <vadhelkemu098@gmail.com>
2025-02-16 10:30:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+    // Trim leading spaces and check for an empty string
+    if (str.trim() === '') {
+        return '';
+    }
+    
+    // Find the index of the first space
+    let spaceIndex = str.indexOf(' ');
+    
+    // If there is no space, return the entire string
+    if (spaceIndex === -1) {
+        return str;
+    }
+    
+    // Otherwise, return the substring from the start to the first space
+    return str.slice(0, spaceIndex);
 }
-
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
